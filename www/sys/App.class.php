140302c8a8c735ad3f60fc497b09d04567e436a5
rafael.zamboni <rafael.zamboni@87968e3b-fc22-a04c-8d99-7786d7fc4fc4>
2015-02-20 10:50:45 +0000

    git-svn-id: https://192.168.1.2:8443/svn/web_monte_santo@22 87968e3b-fc22-a04c-8d99-7786d7fc4fc4

--- www/sys/App.class.php
+++ www/sys/App.class.php
@@ -1,7 +1,7 @@
 <?php
 namespace Sys;
 
-define('VERSION', '15.02.16.01'); //yy.mm.dd.[nn] - nn = número da versão do dia inicio
+define('VERSION', '15.02.20.01'); //yy.mm.dd.[nn] - nn = número da versão do dia inicio
 
 class App
 {
