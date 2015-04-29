ff2b782ec2ade8e8d0bb34d3af632e31f6003b16
thiago.bighetti <thiago.bighetti@87968e3b-fc22-a04c-8d99-7786d7fc4fc4>
2015-04-29 14:44:22 +0000

    Release 15.04.29.01 Ajuste arredondamento 3 casas decimais
    git-svn-id: https://192.168.1.2:8443/svn/web_monte_santo@61 87968e3b-fc22-a04c-8d99-7786d7fc4fc4

--- www/assets/js/helper.js
+++ www/assets/js/helper.js
@@ -7,12 +7,23 @@ function set_focus(obj) {
 	setTimeout(function() { $(obj).focus() }, 700);
 }
 
+Number.prototype.toFixedB = function toFixed ( precision ) {
+    var multiplier = Math.pow( 10, precision + 1 ),
+        wholeNumber = Math.floor( this * multiplier );
+    return (Math.round( wholeNumber / 10 ) * 10 / multiplier).toFixed(precision);
+}
+
+Number.prototype.toFixed10 = function(precision) {
+    return Math.round10(this, -precision).toFixed(precision);
+}
+
 function arredondar3(numero){	
-	return parseFloat((numero).toFixed(4)).toFixed(3);   
-	
+	//return parseFloat((numero).toFixed(4)).toFixed(3);   
+	return (numero).toFixedB(3);   
 }
 
 function nl2br (str, is_xhtml) {   
     var breakTag = (is_xhtml || typeof is_xhtml === 'undefined') ? '<br />' : '<br>';    
     return (str + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1'+ breakTag +'$2');
 }
+
