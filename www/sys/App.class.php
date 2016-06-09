d67f3b4ccc063eea749ea97bea467c9cae21a59a
thiago.bighetti <thiago.bighetti@87968e3b-fc22-a04c-8d99-7786d7fc4fc4>
2016-06-09 13:28:36 +0000

    Release 16.06.09.01
    git-svn-id: https://192.168.1.2:8443/svn/web_monte_santo@115 87968e3b-fc22-a04c-8d99-7786d7fc4fc4

--- www/sys/App.class.php
+++ www/sys/App.class.php
@@ -1,7 +1,7 @@
 <?php
 namespace Sys;
 
-define('VERSION', '15.09.29.01'); //yy.mm.dd.[nn] - nn = número da versão do dia inicio
+define('VERSION', '16.06.09.01'); //yy.mm.dd.[nn] - nn = número da versão do dia inicio
 
 class App
 {
